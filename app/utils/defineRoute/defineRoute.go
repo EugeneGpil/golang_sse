b683044b83f0f69d7d9ec1d95dfd2efa15deab44
EugeneGpil <gpilligrim@gmail.com>
2023-06-28 20:38:37 +0300

    1

--- app/utils/defineRoute/defineRoute.go
+++ app/utils/defineRoute/defineRoute.go
@@ -7,10 +7,29 @@ import (
 )
 
 func Run(httpMethod string, url string, handler func(http.ResponseWriter, *http.Request)) {
+	var lastUrlChar byte
+	if len(url) == 0 {
+		lastUrlChar = '#'
+	} else {
+		lastUrlChar = url[len(url)-1]
+	}
+
+	urlWithoutLastSlash := url
+	if lastUrlChar == '/' {
+		urlWithoutLastSlash = urlWithoutLastSlash[:len(urlWithoutLastSlash)-1]
+	}
+
+	urlWithSlash := urlWithoutLastSlash + "/"
+
+	addRoute(httpMethod, urlWithSlash, handler)
+	addRoute(httpMethod, urlWithoutLastSlash, handler)
+}
+
+func addRoute(method string, url string, handler func(http.ResponseWriter, *http.Request)) {
 	mux := globals.GetMux()
 
 	mux.HandleFunc(url, func(writer http.ResponseWriter, request *http.Request) {
-		if (request.Method != httpMethod) {
+		if request.Method != method {
 			writer.WriteHeader(http.StatusNotFound)
 
 			return
