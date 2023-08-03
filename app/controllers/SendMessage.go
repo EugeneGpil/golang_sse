1d504425d9e6bca29118c87303e911bf8424f1b3
EugeneGpil <gpilligrim@gmail.com>
2023-08-03 03:14:50 +0000

    1

--- app/controllers/SendMessage.go
+++ app/controllers/SendMessage.go
@@ -1,5 +1,19 @@
 package controllers
 
-func SendMessage() {
+import (
+	"net/http"
 
+	"github.com/EugeneGpil/golang_sse/app/globals/sseServer"
+	"github.com/r3labs/sse/v2"
+)
+
+func SendMessage(w http.ResponseWriter, request *http.Request) {
+	query := request.URL.Query()
+
+	streamName := query.Get("stream")
+	message := query.Get("message")
+
+	sseServer.Get().Publish(streamName, &sse.Event{
+		Data: []byte(message),
+	})
 }
--- app/router/routes/SendMessage.go
+++ app/router/routes/SendMessage.go
@@ -3,20 +3,10 @@ package routes
 import (
 	"net/http"
 
-	"github.com/EugeneGpil/golang_sse/app/globals/sseServer"
+	"github.com/EugeneGpil/golang_sse/app/controllers"
 	"github.com/EugeneGpil/router"
-	"github.com/r3labs/sse/v2"
 )
 
 func SendMessage() {
-	router.AddRoute(http.MethodPost, "/message/", func(w http.ResponseWriter, request *http.Request) {
-		query := request.URL.Query()
-
-		streamName := query.Get("stream")
-		message := query.Get("message")
-
-		sseServer.Get().Publish(streamName, &sse.Event{
-			Data: []byte(message),
-		})
-	})
+	router.AddRoute(http.MethodPost, "/message/", controllers.SendMessage)
 }
