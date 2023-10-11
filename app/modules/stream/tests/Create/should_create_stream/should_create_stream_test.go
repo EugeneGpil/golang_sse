7837e5c984967a74ca60df8e537b1ff684ee70c1
EugeneGpil <gpilligrim@gmail.com>
2023-10-11 17:21:00 +0000

    1

--- app/modules/stream/tests/Create/should_create_stream/should_create_stream_test.go
+++ app/modules/stream/tests/Create/should_create_stream/should_create_stream_test.go
@@ -1,10 +1,11 @@
 package should_create_stream
 
 import (
+	"bytes"
+	"encoding/json"
 	"io"
 	"net/http"
 	"net/url"
-	"strings"
 	"testing"
 
 	"github.com/EugeneGpil/golang_sse/app/ship/router/names"
@@ -18,7 +19,7 @@ import (
 
 var mux = tests.GetMux()
 var route = router.ByName(names.StreamCreate)
-var streamName = "message"
+var streamName = "messages"
 
 func Test_should_create_stream(t *testing.T) {
 	tester.SetTester(t)
@@ -26,10 +27,16 @@ func Test_should_create_stream(t *testing.T) {
 	urlObj, err := url.Parse(route.Url)
 	tester.AssertNil(err)
 
+	requestBodyBytes, err := json.Marshal(struct {
+		Stream string
+	} {
+		Stream: streamName,
+	})
+
 	request := http.Request{
 		Method: route.Method,
 		URL:    urlObj,
-		Body: io.NopCloser(strings.NewReader("{\"stream\": \"" + streamName + "\"}")),
+		Body: io.NopCloser(bytes.NewReader(requestBodyBytes)),
 	}
 
 	handler, _ := mux.Handler(&request)
