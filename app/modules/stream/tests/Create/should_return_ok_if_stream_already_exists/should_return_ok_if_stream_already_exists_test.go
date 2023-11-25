445eca4082ba427f4463b1125ac9c8b5c4ea27c7
EugeneGpil <gpilligrim@gmail.com>
2023-11-25 13:09:53 +0000

    1

---
+++ app/modules/stream/tests/Create/should_return_ok_if_stream_already_exists/should_return_ok_if_stream_already_exists_test.go
@@ -0,0 +1,60 @@
+package should_return_ok_if_stream_already_exists
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/EugeneGpil/golang_sse/app/ship/router/names"
+	"github.com/EugeneGpil/golang_sse/app/ship/sseServer"
+	"github.com/EugeneGpil/golang_sse/app/ship/translator"
+	"github.com/EugeneGpil/golang_sse/app/ship/utils/tests/GetMux"
+	"github.com/EugeneGpil/router"
+	"github.com/EugeneGpil/tester"
+
+	httpTester "github.com/EugeneGpil/httpTester"
+)
+
+var mux = GetMux.GetMux()
+var streamName = "the_test_stream"
+var route = router.ByName(names.StreamCreate)
+var expectedMessage = translator.Translate("success")
+
+func Test_should_return_ok_if_stream_already_exists(t *testing.T) {
+	tester.SetTester(t)
+
+	sseServer.Get().CreateStream(streamName)
+
+	isStreamExists := sseServer.Get().StreamExists(streamName)
+
+	tester.AssertSame(isStreamExists, true)
+
+	body := struct{
+		Stream string
+	} {
+		Stream: streamName,
+	}
+
+	response := httpTester.Request(httpTester.GetRequestDto{
+		Method: route.Method,
+		Url:    route.Url,
+		Body:   body,
+	}, mux)
+
+	tester.AssertSame(http.StatusOK, response.GetStatus())
+
+	//TODO tester.assertMessage(response, expectedMessage)
+	responseBody := struct {
+		Message string
+	} {}
+
+	err := response.DecodeBody(&responseBody)
+
+	tester.AssertNil(err)
+
+	tester.AssertSame(expectedMessage, responseBody.Message)
+
+	//TODO tester.assertDoesStreamExists(streamName)
+	doesStreamExistAfterRequest := sseServer.Get().StreamExists(streamName)
+
+	tester.AssertSame(doesStreamExistAfterRequest, true)
+}
