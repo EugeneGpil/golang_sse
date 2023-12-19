2d2e84e61b0035452550db17f808a486c89e7208
EugeneGpil <gpilligrim@gmail.com>
2023-12-19 18:16:45 +0000

    1

--- app/modules/stream/controllers/Listen.go
+++ app/modules/stream/controllers/Listen.go
@@ -3,9 +3,15 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/EugeneGpil/golang_sse/app/modules/stream/validators/StreamValidator"
 	"github.com/EugeneGpil/golang_sse/app/ship/sseServer"
 )
 
 func Listen(writer http.ResponseWriter, request *http.Request) {
+	stream := StreamValidator.New(writer, request).Run()
+	if (stream == "") {
+		return
+	}
+
 	sseServer.Get().ServeHTTP(writer, request)
 }
